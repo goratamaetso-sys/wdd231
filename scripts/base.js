e5111818ccf8c3f233cee331d64576fa75fde537
goratamaetso-sys <goratamaesto@byupathway.edu>
2026-09-04 19:23:25 +0200

    homepage

---
+++ scripts/base.js
@@ -0,0 +1,128 @@
+//store selected items
+const navbutton = document.querySelector("#ham-btn");
+const navlinks = document.querySelector("#nav-bar");
+
+//toggle the show class off and on
+navbutton.addEventListener("click", () => {
+    navbutton.classList.toggle("show");
+    navlinks.classList.toggle("show");
+}); 
+
+//course array list
+const courses = [
+    {
+        subject: 'CSE',
+        number: 110,
+        title: 'Introduction to Programming',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+            'HTML',
+            'CSS'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+        technology: [
+            'C#'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 231,
+        title: 'Frontend Web Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    }
+]
+
+//display courses
+displayCourses(courses);
+
+// Button filters
+document.getElementById("allBtn").addEventListener("click", () => displayCourses(courses));
+document.getElementById("wddBtn").addEventListener("click", () => {
+    const filtered = courses.filter(c => c.subject === "WDD");
+    displayCourses(filtered);
+  });
+document.getElementById("cseBtn").addEventListener("click", () => {
+    const filtered = courses.filter(c => c.subject === "CSE");
+    displayCourses(filtered);
+  });
+
+//display courses function
+function displayCourses(coursesArray){
+    const container = document.getElementById("course-container");
+    const creditTotal = document.getElementById("creditTotal");
+
+    container.innerHTML = "";
+    let totalCredit = 0;
+
+    coursesArray.forEach(course => {
+        const div = document.createElement("div");
+        div.classList.add("course");
+        div.classList.add(course.completed ? "completed": "not-completed");
+        div.textContent = `${course.subject} ${course.number}: ${course.title} (${course.credits} credits) - ${course.completed ? "Completed" : "Not Completed"}`;
+        container.appendChild(div);
+        totalCredit += course.credits;
+    })
+    creditTotal.textContent = `Total Credits: ${totalCredit}`;
+
+}  
+
+//year and last modified
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
